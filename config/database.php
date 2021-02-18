f92fa111b504fd8ecb78fe63253b08c26ef98755
Angel Fabricio <angelfabriciogonzalez@gmail.com>
2021-02-17 21:59:32 -0600

    url conneccion cambiada

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => 'mysql://root:@teamodiosfabri06@161.35.141.24/subeapp',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
