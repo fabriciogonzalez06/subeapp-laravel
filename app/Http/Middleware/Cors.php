56f0a7785aefe43441af82a7d1f420f38d692f15
Angel Fabricio <angelfabriciogonzalez@gmail.com>
2021-02-19 21:52:24 -0600

    se agrego dominio a APP_URL app.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,11 +15,11 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $origin = $request->getHttpHost() == 'localhost' ?
-                    'http://localhost:4200' : 'https://subeapp-demo.web.app';
+        // $origin = $request->getHttpHost() == 'localhost' ?
+        //             'http://localhost:4200' : 'https://subeapp-demo.web.app';
 
         return $next($request)
-      ->header('Access-Control-Allow-Origin', $origin)
+      ->header('Access-Control-Allow-Origin', '*')
       ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
       ->header('Access-Control-Allow-Headers’, ‘X-Requested-With, Content-Type, X-Token-Auth, Authorization');
     }
